b786f6c47e395010387794812c3a93b60c7c187e
bodyavl <88792596+bodyavl@users.noreply.github.com>
2023-03-24 17:33:57 +0200

    simple app created

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import Alert from "./components/Alert";
+
+import Button from "./components/Button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isVisible, setIsVisible] = useState(false);
+  const handleClick = () => {
+    setIsVisible(true);
+  };
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      {isVisible && (
+        <Alert onClose={() => setIsVisible(false)}>
+          Hello <b>Josh</b>
+        </Alert>
+      )}
+      <Button onClick={handleClick}>Open</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    const name = 'Josh'
+    return <h1>Hello {name}</h1>
+}
+
+export default Message;
